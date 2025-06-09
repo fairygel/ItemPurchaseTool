9828ba0871662249ba03db5971fa4e2f839879d2
fairygel <maksimkhvoynitsky@gmail.com>
2025-06-09 06:12:48 +0300

    added + and - buttons to item card(when in cart)

--- src/lwc/itemList/itemList.js
+++ src/lwc/itemList/itemList.js
@@ -4,6 +4,7 @@ import getItemsBySearch from '@salesforce/apex/ItemController.getItemsBySearch';
 import getTypeOptions from '@salesforce/apex/FilterService.getTypeOptions';
 import getFamilyOptions from '@salesforce/apex/FilterService.getFamilyOptions';
 import addItemToCart from '@salesforce/apex/CartController.addItemToCart';
+import getCartItems from '@salesforce/apex/CartController.getCartItems';
 
 export default class ItemList extends LightningElement {
     @track items;
@@ -47,16 +48,51 @@ export default class ItemList extends LightningElement {
     connectedCallback() {
         this.loadPicklistValues();
         this.loadItems();
+        this.loadCartQuantities();
     }
 
-    handleAddToCart(event) {
+    loadCartQuantities() {
+        if (!this.accountId) return;
+
+        getCartItems({ accountId: this.accountId })
+            .then((cartItems) => {
+                const quantities = {};
+                cartItems.forEach((item) => {
+                    quantities[item.Item__c] = item.Quantity__c;
+                });
+                this.cartQuantities = quantities;
+
+                if (this.items) {
+                    this.items = this.items.map(
+                        item => ({
+                            ...item,
+                            quantityInCart: this.cartQuantities[item.Id] || 0
+                        })
+                    );
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    }
+
+    handleChangeQuantity(event) {
+        const shouldSubtract = event.currentTarget.dataset.shouldsubtract === 'true';
         const itemId = event.currentTarget.dataset.id;
-        addItemToCart({ accountId: this.accountId, itemId: itemId })
+
+        addItemToCart({ accountId: this.accountId, itemId: itemId, shouldSubtract: shouldSubtract })
             .then(quantity => {
                 this.cartQuantities = {
                     ...this.cartQuantities,
                     [itemId]: quantity
                 };
+
+                this.items = this.items.map(item => {
+                    if (item.Id === itemId) {
+                        return { ...item, quantityInCart: quantity};
+                    }
+                    return item;
+                })
             })
             .catch(error => {
                 console.error(error);
@@ -88,7 +124,12 @@ export default class ItemList extends LightningElement {
             typeFilters: this.typeFilters
         })
             .then(result => {
-                this.items = result;
+                this.items = result.map(item => {
+                    return {
+                        ...item,
+                        quantityInCart: this.cartQuantities[item.Id] || 0
+                    }
+                })
                 this.error = undefined;
             })
             .catch(error => {
